d8a602ccbf884e0a71d235b110296fdd0ac9f48b
infostics <infosticstech@gmail.com>
2024-03-06 01:19:04 +0530

    added dependecnies

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import langchain_experimental
+import openai
+import pandas
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    print("langchain_experimental version:", langchain_experimental.__version__)
+    print("openai version:", openai.__version__)
+    print("pandas version:", pandas.__version__)
+    return f'Hello, World! { langchain_experimental.__version__, openai.__version__, pandas.__version__}'
 
 # if __name__=='__main__':
 #     app.run()
